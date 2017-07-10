c6e200b9c6128ad4ff05540fde01b25a3271eb16
Christian Reich <christian@rainbow-unicorn.com>
2017-07-10 14:23:48 +0200

    Fixes for safari hero bug isotope layout button :visited color

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,9 @@
 
         Logger.useDefaults();
         Logger.setLevel(Logger.OFF);
+		
+		var browser = ref.getBrowser();
+        $('body').addClass(browser.name.toLowerCase()).addClass('version-' + browser.version.toLowerCase());
 
     };
 
@@ -256,6 +259,28 @@
             e = document.documentElement || document.body;
         }
         return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+    };
+	
+	/*********************
+    get browser type + version
+    *********************/
+    Controller.prototype.getBrowser = function()
+    {
+        var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if(/trident/i.test(M[1])){
+            tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+            return {name:'IE',version:(tem[1]||'')};
+        }
+        if(M[1]==='Chrome'){
+            tem=ua.match(/\bOPR\/(\d+)/)
+            if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }
+        M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+        return {
+            name: M[0],
+            version: M[1]
+        };
     };
 
     window.Controller = Controller;
